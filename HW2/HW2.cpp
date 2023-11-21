84e84a2d228db52c18011daad556053ea16eb0b4
shintanshin <shintanshin@gmail.com>
2023-11-21 08:59:07 +0200

    add HW3 files

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -1,20 +1,72 @@
-// HW2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+
+enum class WeekDay {
+    Zero,
+
+    Monday,
+
+    Tuesday,
+
+    Wednesday,
+
+    Thoursday,
+
+    Friday,
+
+    Saturday,
+
+    Sunday
+};
+void swapVariables()
+{
+    int a = 0;
+    int b = 0;
+    std::cout << "Enter first value: ";
+    std::cin >> a;
+    std::cout << "Enter second value: ";
+    std::cin >> b;
+    int tempValue = a;
+    a = b;
+    b = tempValue;
+    std::cout << "First value: " << a << "\n";
+    std::cout << "Second value: " << b << "\n";
+}
+void doubleToInt()
+{
+    double numDouble = 2548.85;
+    std::cout << numDouble << std::endl;
+    int numInt = int(numDouble);
+    std::cout << numInt << std::endl;
+    std::cout << std::setw(20) << std::right << std::setfill('$') << numDouble << std::endl;
+}
+void difNotation()
+{
+    double num = 25.4885;
+    std::cout << num << std::endl;
+    std::cout << std::scientific << num << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << num << std::endl;
+    std::cout << std::fixed << std::setprecision(5) << num << std::endl;
+    std::cout << std::fixed << std::setprecision(10) << num << std::endl;
+}
+void hw2_weekDays()
+{
+    std::cout << static_cast<int>(WeekDay::Monday) << "\n" << static_cast<int>(WeekDay::Tuesday) << "\n" << static_cast<int>(WeekDay::Wednesday) << "\n" << static_cast<int>(WeekDay::Thoursday)
+        << "\n" << static_cast<int>(WeekDay::Friday) << "\n" << static_cast<int>(WeekDay::Saturday) << "\n" << static_cast<int>(WeekDay::Sunday) << std::endl;
+}
+void checkBool()
+{
+    int a = 0;
+    std::cin >> a;
+    bool b = bool(a);
+    std::cout << std::boolalpha << b << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    swapVariables();
+    doubleToInt();
+    difNotation();
+    hw2_weekDays();
+    checkBool();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ HW3/HW3.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <iomanip>
+
+void gameDuration()
+{
+    int timeInGame = 0;
+    std::cout << "How many tame you spend?: ";
+    std::cin >> timeInGame;
+    int h = static_cast<int>(timeInGame) / 60 / 60;
+    int m = static_cast<int> (timeInGame) / 60 % 60;
+    int s = static_cast<int>(timeInGame) % 60;
+    std::cout << "Your tome in game: " << h << " hours, " << m << " minutes, " << s << " seconds." << std::endl;
+}
+void hw3_2()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    std::cout << "first value:";
+    std::cin >> a;
+    std::cout << "second value:";
+    std::cin >> b;
+    std::cout << "third value:";
+    std::cin >> c;
+    std::cout << a + b + c << std::endl << a * b * c << std::endl << (a + b + c) / static_cast<double>(3) << std::endl;
+    bool compare1 = a < b;
+    bool compare2 = c < b;
+    bool compare3 = a > c;
+    std::cout << "Compare: a < b \n" << std::boolalpha << compare1 << std::endl;
+    std::cout << "Compare: c < b \n" << std::boolalpha << compare2 << std::endl;
+    std::cout << "Compare: a > c \n" << std::boolalpha << compare3 << std::endl;
+}
+void hw3_3()
+{
+    int a = 0;
+    int b = 0;
+    std::cout << "first value:";
+    std::cin >> a;
+    std::cout << "second value:";
+    std::cin >> b;
+    bool lessThan = a < b;
+    bool equal = a == b;
+    bool moreThan = a > b;
+    bool lessOrEqual = a <= b;
+    std::cout << "first value less than second: " << std::boolalpha << lessThan << std::endl;
+    std::cout << "first value equal second: " << std::boolalpha << equal << std::endl;
+    std::cout << "first value more than second: " << std::boolalpha << moreThan << std::endl;
+    std::cout << "first value less or equal second: " << std::boolalpha << lessOrEqual << std::endl;
+
+}
+void hw3_4()
+{
+    int a = 0;
+    int b = 0;
+    std::cout << "Width:";
+    std::cin >> a;
+    std::cout << "Height";
+    std::cin >> b;
+    std::cout << "rectangle area = " << a * b << " m2\n" << "rectangle perimeter = " << 2 * a + 2 * b << " m " << std::endl;
+}
+void hw3_5()
+{
+    const double p = 3.14;
+    int a = 0;
+    std::cout << "Radius:";
+    std::cin >> a;
+    std::cout << "circle area = " << (a * a) * p << " m2\n" << "circle length = " << 2 * p * a << " m ";
+}
+int main()
+{
+    hw3_2();
+}
