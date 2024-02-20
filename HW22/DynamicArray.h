3d1c61da18d7178492333cbb16ec1fc55056dd5b
shintanshin <shintanshin@gmail.com>
2024-02-20 16:24:40 +0200

    Added changes to DynamicArray.h and HW22.cpp files

--- HW22/DynamicArray.h
+++ HW22/DynamicArray.h
@@ -1,45 +1,136 @@
-﻿#pragma once
+#pragma once
 #include <cstdlib>
 
-//#TODO: Make it template class
-//int type should be replace with smth more general
 template<typename T>
-
 class DynamicArray
 {
 public:
     DynamicArray() : data(nullptr), size(0), capacity(0) {}
-    DynamicArray(std::size_t size) : size(size), capacity(size){data = new T[size]();}
-    DynamicArray(const DynamicArray& other) : data(nullptr), size(0), capacity(0) { copyData(other);}
+    DynamicArray(std::size_t size) : size(size), capacity(size) { data = new T[size](); }
+    DynamicArray(const DynamicArray& other) : data(nullptr), size(0), capacity(0) { copyData(other); }
 
-    ~DynamicArray() {delete[] data;}
+    ~DynamicArray() { delete[] data; }
 
-    DynamicArray& operator=(const DynamicArray& other); //assignment operator, #TODO, change signature if needed
+    DynamicArray& operator=(const DynamicArray& other); 
 
-    //validate index always or only in _DEBUG configuration
-    T& operator[](std::size_t index);
+    T& operator[](std::size_t index) {return data[index];}
 
     void setSize(std::size_t newSize);
-    std::size_t getSize() const;
+    std::size_t getSize() const {return size;}
 
     void clear();
 
     void push_back(T element);
 
-
-    //#TODO: HW new methods
-    void pop_back();    //remove last element
-    T back() const;   //get last element
+    void pop_back() { if (size > 0) { --size; } }
+    T back() const;  
 
     void reserve(std::size_t reservedSpace);
-    std::size_t getCapacity() const;    //returns reserve size. Should be ALWAYS >= then actual size
+    std::size_t getCapacity() const { return capacity; }
 
-    void shrinkToFit();
+    void shrinkToFit() { if (size < capacity) { reserve(size); } }
 
-    bool operator==(const DynamicArray& other) const;    //#TODO, change signature if needed
+    bool operator==(const DynamicArray& other) const;  
 private:
-    T* data; 
-    std::size_t size; 
+    T* data;
+    std::size_t size;
     std::size_t capacity;
 
 };
+
+template<typename T>
+DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray& other)
+{
+    if (this != &other)
+    {
+        delete[] data;
+        copyData(other);
+    }
+    return *this;
+}
+
+//template<typename T>
+//T& DynamicArray<T>::operator[](std::size_t index)
+//{
+//    return data[index];
+//}
+
+template<typename T>
+void DynamicArray<T>::setSize(std::size_t newSize)
+{
+    if (newSize <= capacity)
+    {
+        size = newSize;
+    }
+    else
+    {
+        reserve(newSize);
+        size = newSize;
+    }
+}
+
+template<typename T>
+void DynamicArray<T>::clear()
+{
+    delete[] data;
+    data = nullptr;
+    size = 0;
+    capacity = 0;
+}
+
+template<typename T>
+void DynamicArray<T>::push_back(T element)
+{
+    if (size == capacity)
+    {
+        reserve(capacity * 2 + 1);
+    }
+    data[size++] = element;
+}
+
+template<typename T>
+T DynamicArray<T>::back() const
+{
+    if (size > 0)
+    {
+        return data[size - 1];
+    }
+
+
+    throw std::out_of_range("Cannot get back element from an empty array");
+}
+
+template<typename T>
+void DynamicArray<T>::reserve(std::size_t reservedSpace)
+{
+    if (reservedSpace > capacity)
+    {
+        T* newData = new T[reservedSpace];
+        for (std::size_t i = 0; i < size; ++i)
+        {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+        capacity = reservedSpace;
+    }
+}
+
+template<typename T>
+bool DynamicArray<T>::operator==(const DynamicArray& other) const
+{
+    if (size != other.size)
+    {
+        return false;
+    }
+
+    for (std::size_t i = 0; i < size; ++i)
+    {
+        if (data[i] != other.data[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
--- HW22/HW22.cpp
+++ HW22/HW22.cpp
@@ -1,140 +1,9 @@
-﻿#include <iostream>
+#include <iostream>
 #include "DynamicArray.h"
-
-template<typename T>
-DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray& other)
-{
-    if (this != &other)
-    {
-        delete[] data;
-        copyData(other);
-    }
-    return *this;
-}
-
-template<typename T>
-T& DynamicArray<T>::operator[](std::size_t index)
-{
-
-    return data[index];
-}
-
-template<typename T>
-void DynamicArray<T>::setSize(std::size_t newSize)
-{
-    if (newSize <= capacity)
-    {
-        size = newSize;
-    }
-    else
-    {
-        reserve(newSize);
-        size = newSize;
-    }
-}
-
-template<typename T>
-std::size_t DynamicArray<T>::getSize() const
-{
-    return size;
-}
-
-template<typename T>
-void DynamicArray<T>::clear()
-{
-    delete[] data;
-    data = nullptr;
-    size = 0;
-    capacity = 0;
-}
-
-template<typename T>
-void DynamicArray<T>::push_back(T element)
-{
-    if (size == capacity)
-    {
-        reserve(capacity * 2 + 1);
-    }
-    data[size++] = element;
-}
-
-
-
-template<typename T>
-void DynamicArray<T>::pop_back()
-{
-    if (size > 0)
-    {
-        --size;
-    }
-}
-
-template<typename T>
-T DynamicArray<T>::back() const
-{
-    if (size > 0)
-    {
-        return data[size - 1];
-    }
-
-
-    throw std::out_of_range("Cannot get back element from an empty array");
-}
-
-template<typename T>
-void DynamicArray<T>::reserve(std::size_t reservedSpace)
-{
-    if (reservedSpace > capacity)
-    {
-        T* newData = new T[reservedSpace];
-        for (std::size_t i = 0; i < size; ++i)
-        {
-            newData[i] = data[i];
-        }
-        delete[] data;
-        data = newData;
-        capacity = reservedSpace;
-    }
-}
-
-template<typename T>
-std::size_t DynamicArray<T>::getCapacity() const
-{
-    return capacity;
-}
-
-template<typename T>
-void DynamicArray<T>::shrinkToFit()
-{
-    if (size < capacity)
-    {
-        reserve(size);
-    }
-}
-
-template<typename T>
-bool DynamicArray<T>::operator==(const DynamicArray& other) const
-{
-    if (size != other.size)
-    {
-        return false;
-    }
-
-    for (std::size_t i = 0; i < size; ++i)
-    {
-        if (data[i] != other.data[i])
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 
 int main()
 {
-   
+
     DynamicArray<int> m_container;
     m_container.reserve(4);
     m_container.push_back(25); //No new heap allocations, using reserved memory
@@ -147,11 +16,11 @@ int main()
 
     m_container.push_back(55);
 
- 
+
     std::cout << "Size: " << m_container.getSize() << std::endl;
     std::cout << "Capacity: " << m_container.getCapacity() << std::endl;
 
-  
+
     for (std::size_t i = 0; i < m_container.getSize(); ++i)
     {
         std::cout << m_container[i] << " ";
